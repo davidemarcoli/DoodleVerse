df50d07e303d6b4b1037dc6d2874d2622ca8b8e5
davidemarcoli <davide@marcoli.ch>
2022-04-05 11:39:23 +0200

    Made Person Database Table

--- src/main/java/com/dala/data/person/Person.java
+++ src/main/java/com/dala/data/person/Person.java
@@ -1,9 +1,19 @@
 package com.dala.data.person;
 
-import lombok.Data;
+import lombok.*;
 
-@Data
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String firstName;
     private String lastName;
     private double money;
