6dd862119131cab0b1c83e86880f118c9432260b
davidemarcoli <davide@marcoli.ch>
2022-04-12 11:25:25 +0200

    Add Company and Department Generation

---
+++ src/main/java/com/dala/data/company/CompanyRepository.java
@@ -0,0 +1,8 @@
+package com.dala.data.company;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<Company, Long> {
+}
